ee4f64571860d5f3d7dc365f6d37f42c07e2e3ad
Exaltation <michstan96@gmail.com>
2022-08-09 09:18:30 +0700

    Update web routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,14 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(RegisterController::class)->group(function () {
-    Route::get('/register', 'index')->name('register');
-    Route::post('/register', 'validation');
-    Route::get('/{user}/payment', 'viewPayment')->name('payment');
-    Route::post('/{user}/payment', 'checkPayment');
-    Route::post('/{user}/convert', 'convert')->name('convert');
+Route::middleware('guest')->group(function () {
+    Route::controller(RegisterController::class)->group(function () {
+        Route::get('/register', 'index')->name('register');
+        Route::post('/register', 'validation');
+    });
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('/login', 'index')->name('login');
+        Route::post('/login', 'login');
+    });
 });
 
-Route::get('/', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::controller(LoginController::class)->group(function () {
+        Route::get('/{user}/payment', 'viewPayment')->name('payment');
+        Route::post('/{user}/payment', 'checkPayment');
+        Route::post('/{user}/convert', 'convert')->name('convert');
+        Route::post('/logout', 'logout')->name('logout');
+    });
+});
+
+Route::controller(DashboardController::class)->group(function () {
+    Route::get('/', 'index')->name('dashboard');
+});
