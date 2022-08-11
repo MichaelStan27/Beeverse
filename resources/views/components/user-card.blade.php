6a4866606ba915e3da06c5a8a1046588239fea29
Exaltation <michstan96@gmail.com>
2022-08-11 23:09:09 +0700

    Improve styling on user card -make collection empty text looks better in profile page

--- resources/views/components/user-card.blade.php
+++ resources/views/components/user-card.blade.php
@@ -18,7 +18,9 @@
                                 <img src="{{ asset('assets/avatars') }}/{{ $collection->avatar->image }}"
                                     style="width: 4rem">
                             @empty
-                                <h5 class="text-secondary">{{ $user->name }}'s collection is empty</h5>
+                                <h5 class="text-secondary border w-100">{{ $user->name }}'s collection
+                                    is empty
+                                </h5>
                             @endforelse
                         </div>
                     </div>
