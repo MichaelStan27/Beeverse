2d124c25e27c7f0254bf5031467ced8b24d34ace
Exaltation <michstan96@gmail.com>
2022-08-12 14:44:46 +0700

    keep session when logout to keep website lang

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -72,10 +72,6 @@ class LoginController extends Controller
     {
         Auth::logout();
 
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
         return redirect()->route('dashboard')->with('message', 'Logout successful!');
     }
 }
